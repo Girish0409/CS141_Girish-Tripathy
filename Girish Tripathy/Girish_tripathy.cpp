9d202a75847beabd12c3fd5c7de2a4600772f784
Girish0409 <42180376+Girish0409@users.noreply.github.com>
2018-08-07 23:50:18 +0530

    Initial commit

---
+++ Girish Tripathy/Girish_tripathy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+      cout << "Namaskar Duniyavaalo" ;
+      cout << "This is my First Cpp program" ;
+      cout << "Girish Tripathy" ;
+}
+
+
+
